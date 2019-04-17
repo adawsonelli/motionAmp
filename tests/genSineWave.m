bca5a2b594d3626597a7ef88eeaf335804664499
Alex Dawson-Elli <dawsonelli@wisc.edu>
2019-04-17 14:27:31 -0500

    added python code for generating a test image

--- tests/genSineWave.m
+++ tests/genSineWave.m
@@ -10,7 +10,51 @@ function [vid] = genSineWave(size,wl,amp)
 
 %% generate the sinewave
 
+"""
+make a test video for evaluating motion amplification of a pure sign wave
+"""
+
+def genTestVideo():
+    """
+    adjust function signature in future...
+    return: 3d np.array [x,y,t]
+    """
+    # define properties of the wave functions
+
+    # wavelengths
+    lambda_x = 20  #wavelength is [pixels / cycle]
+    lambda_y = 100  #wavelength in [pixels / cycle]
+    f_t = 2         #spacial frequency in [Hz]
+
+    #amplitudes
+    Ax = 127.5      #[gray levels]
+    Ay = 0          #[pixels]
+    At = 100        #[pixels]
+
+    # initialize array of the appropriate size
+    x = 800; y = 600 ;  t = 10 ; dt = .01
+    tVec = np.arange(0,t,dt)
+    xVec = np.arange(0,x)
+    yVec = np.arange(0,y)
+
+    video = np.zeros((x,y,len(tVec)), dtype = np.uint8)
+
+    # populate array according to specified resolution and frequency
+    for t in range(len(tVec)- 1):
+        phi = np.sin(t/100)
+
+        #populate video frame
+        for row in range(y):
+            video[:,row,t] = Ax * np.sin(xVec/lambda_x + .2*phi) + Ax
+
+
+    return video
+
+tst = genTestVideo()
+print(tst.shape)
+
+
+
 %duplicate it into 3 channels.
 
 end
-
