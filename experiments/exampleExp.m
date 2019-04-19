41ba752381866b3e09fc78abefc1a56acbd9b140
Alex <dawsonelli@wisc.edu>
2019-04-19 11:11:02 -0500

    added functions for importing .mp4 video into a 4D - array, downsampling and exporting video.

--- experiments/exampleExp.m
+++ experiments/exampleExp.m
@@ -4,15 +4,39 @@
 
 %inital config vars
 videoName = 'drawStringTest';
+rsName = strcat(videoName,'_resampled');
 
 %import video into a 4d matrix [y,x,t,color]
+vid = utils.importVid(videoName);
 
-%downsample video to the appropriate y,x size
+%% downsample video to the appropriate y,x size
+% select the downsampling method of your choosing
+dsMethod = 0;
+height = 600; width = 800; sf = .5;
+
+switch dsMethod
+    %downsample by scale factor
+    case 0
+        vid = imresize(vid, .5);
+    %maintain aspect ratio, fix height  
+    case 1
+        vid = imresize(vid, [height NaN]);
+    %maintain aspect ratio, fix width
+    case 2
+        vid = imresize(vid, [NaN width]);
+    %change aspect ratio
+    case 3
+        vid = imresize(vid, [height width]);
+        
+end
 
 %save the downsized video to disk
-% strcat(videoName,'_resampled')
+utils.saveVid(vid,rsName);
 
 %% amplify motion within the video
+
+%import video
+vid = utils.importVid(rsName);
 
 %init and config motionAmpConfig structure
 
---
+++ utils/utils.m
@@ -0,0 +1,57 @@
+classdef utils
+    %UTILS Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods(Static)
+        function vid = importVid(fileName)
+            %import a video from the data directory into the workspace as a
+            %4D - array
+            
+            %init VideoReader object
+            vidPath = strcat('../data/',fileName,'.mp4');
+            vr = VideoReader(vidPath);
+            nColorChannels = 3;
+            vid = zeros(vr.Height,vr.Width,vr.NumberOfFrames,nColorChannels);
+            
+            %have to recreate vr after reading numberofframes....wierd
+            vr = VideoReader(vidPath);
+            
+            %read frames into vid
+            fr = 1;
+            while hasFrame(vr)
+                vid(:,:,fr,:) = readFrame(vr);
+                fr = fr + 1;
+            end
+           
+        end
+        
+        function saveVid(vid,fileName)
+            %saves a 4D array of the form [y,x,t,color] into a .mp4 file in
+            %the data directory
+            
+            
+            %setup video writer
+            fn = strcat('../data/',fileName,'.mp4');
+            v = VideoWriter(fn,'MPEG-4');
+            frames = size(vid,3);
+            
+            %write each frame
+            open(v)
+            for f = 1:frames
+                frame = uint8(squeeze(vid(:,:,f,:)));
+                writeVideo(v,frame)
+            end
+            close(v)
+                
+            
+            
+        end
+        
+            
+    end
+    
+end
+
